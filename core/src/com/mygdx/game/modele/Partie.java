17d00438a585002595e73fa013f53f3c0452be0d
Pichollet Laurent <lpicholl@abe-a-04.enssat.fr>
2017-12-05 17:59:28 +0100

    Ajout de partie

---
+++ core/src/com/mygdx/game/modele/Partie.java
@@ -0,0 +1,19 @@
+package com.mygdx.game.modele;
+
+public class Partie {
+	
+	public static final int NOMBRE_NAVIRES = 2;
+	private String joueur1Nom;
+	private Navire joueur1Navires[];
+	private String joueur2Nom;
+	private Navire joueur2Navires[];
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
