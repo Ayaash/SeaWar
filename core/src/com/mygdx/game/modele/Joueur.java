97c9eda4410c5434ed5a6306aa31a640b32e3b94
Vneintal <valentinlouveau72@gmail.com>
2017-12-07 22:09:00 +0100

    recolor

--- core/src/com/mygdx/game/modele/Joueur.java
+++ core/src/com/mygdx/game/modele/Joueur.java
@@ -8,6 +8,11 @@ public class Joueur {
 	protected Navire navires[];
 	private int id;
 	
+	
+	private float R;
+	private float V;
+	private float B;
+	
 	public String getNom(){
 		return nom;
 	}
@@ -30,5 +35,16 @@ public class Joueur {
 		this.nom = nom;
 		this.navires = navires.clone();
 		this.id = id;
+		R=1;V=1;B=1;
+	}
+	
+	public void setColor(float R,float V,float B){
+		this.R=R;
+		this.V=V;
+		this.B=B;
+		int i=0;
+		for(i=0;i<navires.length;i++){
+			navires[i].setColor(R, V, B, 1);
+		}
 	}
 }
