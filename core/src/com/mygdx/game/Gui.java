555906f3875041fe9c1401662a3ce2cd2185e730
Ayaash <hugo@ayaash.me>
2017-12-13 18:16:39 +0100

    debut de scene

--- core/src/com/mygdx/game/Gui.java
+++ core/src/com/mygdx/game/Gui.java
@@ -1,63 +1,24 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationListener;
-import com.badlogic.gdx.InputProcessor;
+//import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.mygdx.game.modele.Plateau;
 
-public class Gui implements ApplicationListener, InputProcessor {
+import core.mygdx.game.actor.graphPlateau;
+
+public class Gui implements ApplicationListener {
 	
+	private Viewport viewport;
+	private Stage stageJeu; 
 	
-
-	@Override
-	public boolean keyDown(int keycode) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean keyUp(int keycode) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean keyTyped(char character) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean touchDragged(int screenX, int screenY, int pointer) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean mouseMoved(int screenX, int screenY) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean scrolled(int amount) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
 	@Override
 	public void create() {
-		// TODO Auto-generated method stub
+		viewport = new ScreenViewport();
+		stageJeu = new Stage(viewport);
+		stageJeu.addActor(new graphPlateau(Plateau.getInstance()));
 
 	}
 
---
+++ core/src/core/mygdx/game/actor/graphPlateau.java
@@ -0,0 +1,13 @@
+package core.mygdx.game.actor;
+
+import com.badlogic.gdx.scenes.scene2d.Group;
+import com.mygdx.game.modele.Plateau;
+
+public class graphPlateau extends Group {
+	private Plateau plateau;
+	
+	public graphPlateau(Plateau plateau) {
+		this.plateau = plateau;
+	}
+
+}
