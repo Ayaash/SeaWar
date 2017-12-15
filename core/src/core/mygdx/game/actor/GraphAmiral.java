ee74cd59be41fd3907a8796241171352ea340241
Ayaash <hugo@ayaash.me>
2017-12-15 15:53:01 +0100

    merge

---
+++ core/src/core/mygdx/game/actor/GraphAmiral.java
@@ -0,0 +1,11 @@
+package core.mygdx.game.actor;
+
+import com.mygdx.game.modele.Amiral;
+
+public class GraphAmiral extends GraphNavire {
+
+	public GraphAmiral(Amiral _navire) {
+		super(_navire);
+	}
+
+}
