c897f387c75e57e3097bfd9f708ffe24c1c2d20b
Pichollet Laurent <lpicholl@abe-a-13.enssat.fr>
2017-12-22 15:54:25 +0100

    IA aleatoire en theorie fonctionnelle

--- core/src/com/mygdx/game/ia/Coup.java
+++ core/src/com/mygdx/game/ia/Coup.java
@@ -1,17 +1,43 @@
 package com.mygdx.game.ia;
 
+import com.mygdx.game.modele.Partie;
+
 public class Coup {
 
 	private TypeCoup typeCoup;
 	private int coordonnees[];
+	private int degats;
 	
 	public Coup(TypeCoup type, int coordonnees[]){
 		this.typeCoup = type;
 		this.coordonnees = coordonnees.clone();
 	}
+	public Coup(TypeCoup type){
+		this.typeCoup = type;
+	}
+	public Coup(TypeCoup type, int coordonnees[], int degats){
+		this.typeCoup = type;
+		this.coordonnees = coordonnees.clone();
+		this.degats = degats;
+	}
 	
-	public void faireCoup(){
-		
+	public void faireCoup(Partie p){
+		switch(typeCoup){
+		case FinTour:
+			p.finirTourNavire();
+			break;
+		case Mouvement:
+			p.deplacerNavire(coordonnees);
+			break;
+		case TirPrincipal:
+			p.tirerSurUneCase(coordonnees, degats);
+			p.demanderTirsPossiblesPrincipal();
+			break;
+		case TirSecondaire:
+			p.tirerSurUneCase(coordonnees, degats);
+			p.demanderTirsPossiblesSecondaire();
+			break;
+		}
 	}
 	
 	
--- core/src/com/mygdx/game/ia/IAAleatoire.java
+++ core/src/com/mygdx/game/ia/IAAleatoire.java
@@ -1,7 +1,10 @@
 package com.mygdx.game.ia;
 
+import java.util.Random;
+
 import com.mygdx.game.modele.Navire;
 import com.mygdx.game.modele.Partie;
+import com.mygdx.game.modele.Tir;
 
 public class IAAleatoire extends AbstractIA {
 
@@ -15,6 +18,9 @@ public class IAAleatoire extends AbstractIA {
 		Coup meilleur = null;
 		Partie copie = copiePartie();
 		int mouvement[][] = null;
+		Tir TirPrincipal = null;
+		Tir TirSecondaire = null;
+		boolean finTour = false;
 		
 		//Deplacements
 		if(!copie.peutSeDeplacer() && !copie.sEstDeplace()){
@@ -29,18 +35,50 @@ public class IAAleatoire extends AbstractIA {
 		}
 		
 		//Tirs
-		//if(copie.)
-		
-		
-		
-		
-		
-		
-		Navire navire = partie.getNavireCourant();
-		boolean peutBouger = navire.deplacementsRestants() != 0;
-		if(peutBouger){
-			mouvement = navire.deplacementsPossibles();
+		if(copie.peutTirerPrincipal()){
+			TirPrincipal = copie.demanderTirsPossiblesPrincipal();
+			copie = copiePartie();
 		}
+		if(copie.peutTirerSecondaire()){
+			TirSecondaire = copie.demanderTirsPossiblesSecondaire();
+			copie = copiePartie();
+		}
+		
+		//Fin du tour
+		if(copie.sEstDeplace()){
+			finTour = true;
+		}
+		
+		int nbCoupPossibles = 0;
+		int tailleMouvement = 0;
+		int tailleTirP = 0;
+		int tailleTirS = 0;
+		if(mouvement != null) tailleMouvement = mouvement.length;
+		if(TirPrincipal != null) tailleTirP = TirPrincipal.cases.length;
+		if(TirSecondaire != null) tailleTirS = TirSecondaire.cases.length;
+		if(finTour) nbCoupPossibles += 1;
+		
+		nbCoupPossibles += (tailleMouvement + tailleTirP + tailleTirS);
+		
+		if(nbCoupPossibles == 0) return null; //Aucun coup possible, c'est impossible
+
+		Random rand = new Random();
+		int selection = rand.nextInt(nbCoupPossibles + 1);
+		if(selection < tailleMouvement){
+			meilleur = new Coup(TypeCoup.Mouvement, mouvement[selection-1]);
+		}else if(selection < (tailleMouvement + tailleTirP )){
+			meilleur = new Coup(TypeCoup.TirPrincipal,
+					TirPrincipal.cases[selection-tailleMouvement-1],
+					TirPrincipal.degats
+					);
+		}else if(selection < (tailleMouvement + tailleTirP + tailleTirS )){
+			meilleur = new Coup(TypeCoup.TirSecondaire, 
+					TirSecondaire.cases[selection-tailleMouvement-tailleTirP-1],
+					TirSecondaire.degats);
+		}else{
+			meilleur = new Coup(TypeCoup.FinTour);
+		}
+
 		
 		return meilleur;
 	}
