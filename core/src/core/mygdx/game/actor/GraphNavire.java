6481f8b8ce24c60080c12559d81af86e0cc49d2d
Louveau Valentin <vlouveau@abe-a-17.enssat.fr>
2018-01-17 08:54:37 +0100

    Code un peu plus propre 1

--- core/src/core/mygdx/game/actor/GraphNavire.java
+++ core/src/core/mygdx/game/actor/GraphNavire.java
@@ -11,7 +11,7 @@ import com.mygdx.game.modele.Navire;
 import com.mygdx.game.modele.Plateau;
 import com.mygdx.game.modele.Textures;
 
-public abstract class GraphNavire extends Image { // TODO passer en Image
+public abstract class GraphNavire extends Image {
 	private Navire m_navire;
 	
 	protected float m_x;
@@ -34,50 +34,31 @@ public abstract class GraphNavire extends Image { // TODO passer en Image
 		this.m_navire = _navire;
 
 		actualizePosSize();
-		//this.setBounds(m_x, m_y, m_w, m_h);
 		this.toFront();
 	}
 
 
 	protected void actualizePosSize(){
-		//posX=(int) position[0]*10;
-		//posY=(int) position[1]*10;
 		double sx=(Gui.maxWX-Gui.minWX+0f)/(Plateau.TAILLE_HORIZONTALE+0f);
 		double sy=(Gui.maxWY-Gui.minWY+0f)/(Plateau.TAILLE_VERTICALE+0f);
 		
 		
 		Case c=m_navire.getPlateau().getCases(m_navire.getPosition()[0], m_navire.getPosition()[1]);
 		
-		m_w=(int) (sx/0.8);//Pour emboiter les hexagones
-		m_h=(int) (sy/1);
-		
+		m_w=(int) (sx/0.78);//Pour emboiter les hexagones
+		m_h=(int) (sy/1.05);		
 		this.m_x=(int) (Gui.minWX  +  c.getPosition()[0]*sx );
-		//this.m_y=(int) ( Gui.maxWY+Gui.minWY-1.5*m_h-(Gui.minWY  + wy*sy ) );
 		this.m_y=(int) (Gui.minWY  +  c.getPosition()[1]*sy);
-		
-		
-		
-		//posX=(int) Jeu.minWX + (Jeu.maxWX-Jeu.minWX)*position[0];
-		//posY=(int) Jeu.minWY + (Jeu.maxWX-Jeu.minWX)*position[1];
 
-		if( c.getPosition()[0]%2==0){//TODO Peut etre revoir la paritï¿½
+		if( c.getPosition()[0]%2==0){
 			m_y+=sy/2f;
-			//System.out.println(1);
-		}else{
-			//System.out.println(2);
-
 		}
 	
-		
-		
-		//this.actualizeSprite(obj);
 	}
 	
 	/**envoie true ssi la selection a bien eu lieu*/
 	public boolean select(){
-		//this.setColor(SELECTEDCOLOR);
 		boolean btmp;
-		//((GraphPlateau)this.getParent().getParent()).deselectAllNavire();//un seul navire selectionn�e a la fois
 		btmp=((GraphPlateau)this.getParent()).selectNavire(m_navire);//un seul navire selectionn�e a la fois
 		if(btmp){
 			selected=true;
@@ -86,7 +67,6 @@ public abstract class GraphNavire extends Image { // TODO passer en Image
 		return false;
 	}
 	public void deselect(){
-		//this.setColor(BASECOLOR);
 		selected=false;
 	}
 
@@ -113,7 +93,6 @@ public abstract class GraphNavire extends Image { // TODO passer en Image
 		private int m_h;
 		private int m_w;
 		private Navire n;
-		private GraphNavire gn;
 
 		
 		public Drawbt(int _x,int _y,Navire _n){
@@ -130,11 +109,6 @@ public abstract class GraphNavire extends Image { // TODO passer en Image
 		
 		protected void actualizePosSize(){
 
-			Case c= n.getPlateau().getCases(n.getPosition()[0], n.getPosition()[1]); 
-			
-			if(gn==null){
-				gn=GraphPlateau.getMainInstance().getGraphNavire(c.getPosition()[0], c.getPosition()[1]);
-			}
 			
 			
 			double sx=(Gui.maxWX-Gui.minWX+0f)/(Plateau.TAILLE_HORIZONTALE+0f);
@@ -145,7 +119,6 @@ public abstract class GraphNavire extends Image { // TODO passer en Image
 			m_h=(int) (sy/1.05);
 			
 			this.m_x=(int) (Gui.minWX  +  wx*sx );
-			//this.m_y=(int) (Gui.minWY  +  wy*sy );
 			this.m_y=(int) (Gui.maxWY  -  wy*sy);
 
 
@@ -161,7 +134,7 @@ public abstract class GraphNavire extends Image { // TODO passer en Image
 		public void draw(Batch batch, float x, float y, float width,
 				float height) {
 			
-			if(n.getVie()>0){
+			if(!n.estMort()){
 
 				actualizePosSize();
 	
@@ -183,26 +156,22 @@ public abstract class GraphNavire extends Image { // TODO passer en Image
 	
 		@Override
 		public float getLeftWidth() {
-			// TODO Auto-generated method stub
 			return m_x;
 		}
 	
 		@Override
 		public void setLeftWidth(float leftWidth) {
-			// TODO Auto-generated method stub
 			m_x=(int) leftWidth;
 			
 		}
 	
 		@Override
 		public float getRightWidth() {
-			// TODO Auto-generated method stub
 			return m_x+m_w;
 		}
 	
 		@Override
 		public void setRightWidth(float rightWidth) {
-			// TODO Auto-generated method stub
 			m_w=(int) rightWidth-m_x;
 
 			
@@ -210,52 +179,44 @@ public abstract class GraphNavire extends Image { // TODO passer en Image
 	
 		@Override
 		public float getTopHeight() {
-			// TODO Auto-generated method stub
 			return  m_y;
 		}
 	
 		@Override
 		public void setTopHeight(float topHeight) {
-			// TODO Auto-generated method stub
 			m_h=(int) topHeight-m_y;
 			
 		}
 	
 		@Override
 		public float getBottomHeight() {
-			// TODO Auto-generated method stub
 			return m_y+m_h/2;
 		}
 	
 		@Override
 		public void setBottomHeight(float bottomHeight) {
-			// TODO Auto-generated method stub
 			m_y=(int) bottomHeight;
 
 		}
 	
 		@Override
 		public float getMinWidth() {
-			// TODO Auto-generated method stub
 			return m_w;
 		}
 	
 		@Override
 		public void setMinWidth(float minWidth) {
-			// TODO Auto-generated method stub
 			m_w=(int) minWidth;
 			
 		}
 	
 		@Override
 		public float getMinHeight() {
-			// TODO Auto-generated method stub
 			return m_h;
 		}
 	
 		@Override
 		public void setMinHeight(float minHeight) {
-			// TODO Auto-generated method stub
 			m_h=(int) minHeight;
 
 		}
